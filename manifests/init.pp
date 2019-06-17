285511972b8c45e77d877d5a250286f54084e79b
Jordi Prats <jordi.prats@gmail.com>
2019-06-17 12:26:04 +0200

    fix acceptance

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@ class python(
               $manage_package = true,
               $manage_config  = true,
             ) inherits python::params {
+
   class { '::python::install': } ->
   class { '::python::config': } ->
   Class['::python']
