339e66ed6d9d22186b965007a6121ef966e9fa64
Jordi Prats <jordi.prats@gmail.com>
2019-06-18 14:01:29 +0200

    demo sense index_url

--- examples/demo_resouce_custom_index.pp
+++ examples/demo_resouce_custom_index.pp
@@ -1,5 +1,9 @@
 include ::python
 
+pythonpip { 'penisipsum':
+  ensure       => 'present',
+}
+
 pythonpip { 'nagiosplugin':
   ensure       => 'present',
   index_url    => 'http://pipmirror.cm.nttcom.ms',
